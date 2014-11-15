e7d92976af0b7737a620527db18e53927382bd5f
neonwarge04 <magnumneon04@hotmail.com>
2014-11-16 06:08:17 +0800

    changes directory

--- GameApplication.cpp
+++ GameApplication.cpp
@@ -174,15 +174,15 @@ GameApplication::GameState GameApplication::gameLoop()
 
 bool GameApplication::loadGameAssets()
 {
-	textures->addPath("blue_block"	 , "asset/textures/blue_block.png"		);
-	textures->addPath("cyan_block"	 , "asset/textures/cyan_block.png"		);
-	textures->addPath("green_block"	 , "asset/textures/green_block.png"		);
-	textures->addPath("orange_block" , "asset/textures/orange_block.png"	);
-	textures->addPath("purple_block" , "asset/textures/purple_block.png"	);
-	textures->addPath("red_block"	 , "asset/textures/red_block.png"		);
-	textures->addPath("yellow_block" , "asset/textures/yellow_block.png"	);
-	textures->addPath("marked_block" , "asset/textures/marked_block.png"	);
-	textures->addPath("ghost_block"	 , "asset/textures/ghost_block.png"		);
+	textures->addPath("blue_block"	 , "content/asset/textures/blue_block.png"		);
+	textures->addPath("cyan_block"	 , "content/asset/textures/cyan_block.png"		);
+	textures->addPath("green_block"	 , "content/asset/textures/green_block.png"		);
+	textures->addPath("orange_block" , "content/asset/textures/orange_block.png"	);
+	textures->addPath("purple_block" , "content/asset/textures/purple_block.png"	);
+	textures->addPath("red_block"	 , "content/asset/textures/red_block.png"		);
+	textures->addPath("yellow_block" , "content/asset/textures/yellow_block.png"	);
+	textures->addPath("marked_block" , "content/asset/textures/marked_block.png"	);
+	textures->addPath("ghost_block"	 , "content/asset/textures/ghost_block.png"		);
 
 	if (!textures->checkAllAssets())
 		return false;
